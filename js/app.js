1e3d6f8e8f53e61daf8a884f6368e7853cfb883f
akominiak <kominiak.adrian@gmail.com>
2019-11-29 01:25:26 +0100

    changed background and added styles

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const tableMenu = document.querySelector('.tableForecastMenu');
+const mapMenu = document.querySelector('.mapForecastMenu');
+const details = document.querySelector('.details');
+const weather = document.querySelector('.weather');
+
+tableMenu.addEventListener('click', () => {
+    tableMenu.classList.add('active');
+    mapMenu.classList.remove('active');
+    details.classList.remove('off');
+    weather.classList.add('off');
+})
+
+mapMenu.addEventListener('click', () => {
+    mapMenu.classList.add('active');
+    tableMenu.classList.remove('active');
+    weather.classList.remove('off');
+    details.classList.add('off');
+
+})
--- js/search.js
+++ js/search.js
@@ -1,52 +1,49 @@
-const searching=document.querySelector('.searchCity'); //Pole wyszukiwarki
-const suggestions=document.querySelector('.suggestion'); //Pole dopasowanych miast
+const searching = document.querySelector('.searchCity'); //Pole wyszukiwarki
+const suggestions = document.querySelector('.suggestion'); //Pole dopasowanych miast
 
-const cities=[]; //tablica, do ktorej wrzucimy dane otrzymane z APi
+const cities = []; //tablica, do ktorej wrzucimy dane otrzymane z APi
 
 fetch("https://danepubliczne.imgw.pl/api/data/synop")
-    .then(resp=>resp.json())
-    .then(resp=>cities.push(...resp));
+    .then(resp => resp.json())
+    .then(resp => cities.push(...resp));
 
-function findCity(word,city){  
-    return city.filter(place=>{
-        const regex=new RegExp(word,'gi');
+function findCity(word, city) {
+    return city.filter(place => {
+        const regex = new RegExp(word, 'gi');
         return place.stacja.match(regex);
     })
-    
+}
+
+//Funkcja, która w miejsce tekstu wyszukiwarki wstawia klikniete miasto
+function insertIntoSearching(parametr) {
+    searching.value = parametr.innerHTML;
 }
 
 
- //Funkcja, która w miejsce tekstu wyszukiwarki wstawia klikniete miasto
-function insertIntoSearching(parametr){
-searching.value=parametr.innerHTML;
-}
-
-
-function displayCities(){
+function displayCities() {
     /*
     matchArray to tablica zawierająca miasta, których nazwa zawiera
     znaki wpisane w wyszukiwarce.
     Funkcja findCity to funkcja, która tworzy tą tablicę.
     */
-    const matchArray=findCity(this.value,cities); 
+    const matchArray = findCity(this.value, cities);
 
     //Poniżej następuje tworzenie listy proponowanych miast
     //Na podstawie dopasowania, które nastąpiło wyżej
-    const html=matchArray.map(city=>{ 
-        return`
+    const html = matchArray.map(city => {
+        return `
         <li>
         <span class ="name" onclick="insertIntoSearching(this)" " >${city.stacja}</span>
         </li>
         `;
     }).join('');
-
     //Dodawanie listy dopasowanych miast 
-    suggestions.innerHTML=html;
-    
-if(searching.value==""){
-    suggestions.innerHTML=`<li><span class ="name">Brak wyników</span></li>`;
-}
+    suggestions.innerHTML = html;
+
+    if (searching.value == "") {
+        suggestions.innerHTML = `<li><span class ="name">Brak wyników</span></li>`;
+    }
 }
 
 
-searching.addEventListener('keyup',displayCities);
+searching.addEventListener('keyup', displayCities);
